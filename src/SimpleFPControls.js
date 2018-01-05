9b3a70f97ed03fabadfcbb02ee22e932cf9913f9
mprafson <mprafson@google.com>
2018-01-05 15:40:25 -0800

    added ar commands

--- src/SimpleFPControls.js
+++ src/SimpleFPControls.js
@@ -2,6 +2,13 @@ import { Vector3, Object3D } from 'three';
 
 const PI_2 = Math.PI / 2;
 
+const MOVE_UP = 81; // e
+const MOVE_DOWN = 69; // q
+const PITCH_UP = 73; // i
+const PITCH_DOWN = 75; // k
+const YAW_LEFT = 74; // j
+const YAW_RIGHT = 76; // l
+
 /**
  * Chunks taken from PointerLockControls and PointerLockControls demo.
  *
@@ -13,8 +20,8 @@ export default class SimpleFPControls {
     this.object = object;
     this.enabled = true;
     this.movementSpeed = 50.0;
-    this.lookSpeedX = 3;
-    this.lookSpeedY = 2;
+    this.lookSpeedX = 30.0;
+    this.lookSpeedY = 30.0;
 
     this.pitch = new Object3D();
     this.yaw = new Object3D();
@@ -25,6 +32,12 @@ export default class SimpleFPControls {
     this.moveBackward = false;
     this.moveLeft = false;
     this.moveRight = false;
+    this.moveUp = false;
+    this.moveDown = false;
+    this.pitchUp = false;
+    this.pitchDown = false;
+    this.yawLeft = false;
+    this.yawRight = false;
 
     this.velocity = new Vector3();
     this.direction = new Vector3();
@@ -32,6 +45,7 @@ export default class SimpleFPControls {
     this.onMouseMove = this.onMouseMove.bind(this);
     this.onKeyDown = this.onKeyDown.bind(this);
     this.onKeyUp = this.onKeyUp.bind(this);
+
     document.addEventListener('mousemove', this.onMouseMove, false);
     document.addEventListener('keyup', this.onKeyUp, false);
     document.addEventListener('keydown', this.onKeyDown, false);
@@ -75,6 +89,15 @@ export default class SimpleFPControls {
 
       case 39: /*right*/
       case 68: /*D*/ this.moveRight = true; break;
+
+      case MOVE_UP: this.moveUp = true; break;
+      case MOVE_DOWN: this.moveDown = true; break;
+
+      case PITCH_UP: this.pitchUp = true; break;
+      case PITCH_DOWN: this.pitchDown = true; break;
+
+      case YAW_LEFT: this.yawLeft = true; break;
+      case YAW_RIGHT: this.yawRight = true; break;
     }
   }
 
@@ -93,6 +116,15 @@ export default class SimpleFPControls {
 
       case 39: /*right*/
       case 68: /*D*/ this.moveRight = false; break;
+
+      case MOVE_UP: this.moveUp = false; break;
+      case MOVE_DOWN: this.moveDown = false; break;
+
+      case PITCH_UP: this.pitchUp = false; break;
+      case PITCH_DOWN: this.pitchDown = false; break;
+
+      case YAW_LEFT: this.yawLeft = false; break;
+      case YAW_RIGHT: this.yawRight = false; break;
     }
   }
 
@@ -110,7 +142,20 @@ export default class SimpleFPControls {
       this.velocity.x -= this.direction.x * this.movementSpeed * delta;
     }
 
+    if (this.pitchUp || this.pitchDown) {
+      this.pitch.rotation.x -= (Number(this.pitchDown) - Number(this.pitchUp)) * this.lookSpeedY / 1000;
+      this.pitch.rotation.x = Math.max(-PI_2, Math.min(PI_2, this.pitch.rotation.x));
+    }
+
+    if (this.yawLeft || this.yawRight) {
+      this.yaw.rotation.y -= (Number(this.yawRight) - Number(this.yawLeft)) * this.lookSpeedX / 1000;
+    }
+
     this.yaw.translateX(this.velocity.x * delta);
     this.yaw.translateZ(this.velocity.z * delta);
+
+    if (this.moveUp || this.moveDown) {
+      this.object.position.y -= (Number(this.moveUp) - Number(this.moveDown)) / 25;
+    }
   }
 }
